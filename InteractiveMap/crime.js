72952cd336bf6b08955131bdc92e7f7b86c0fd37
WadoodMalik700 <s.hussain0120231@arts.ac.uk>
2026-05-06 16:05:38 +0100

    Add crime data visualization for selected area

--- InteractiveMap/crime.js
+++ InteractiveMap/crime.js
@@ -1,1 +1,26 @@
+const params = new URLSearchParams(window.location.search);
+const area = params.get("area");
 
+document.getElementById("title").innerText = area + " Crime Breakdown";
+
+const crimeData = {
+    Camden: [30, 20, 15, 10],
+    Greenwich: [15, 10, 8, 5],
+    Hackney: [25, 20, 15, 10],
+    Westminster: [40, 30, 20, 15],
+    Croydon: [20, 15, 10, 5]
+};
+
+const labels = ["Theft", "Violence", "Burglary", "Other"];
+
+const ctx = document.getElementById('crimeChart');
+
+new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: labels,
+        datasets: [{
+            data: crimeData[area] || [10,10,10,10]
+        }]
+    }
+});
